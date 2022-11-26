46532d4c506c2a03ac76f8b7bcf0126e1d7f6c73
Enock97 <enockladu@hotmail.com>
2022-11-26 08:37:02 +0100

    Add project files.

---
+++ Kunde-SPA-Routing/DAL/IObservasjonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EksamenVersjon3.Model;
+using KundeApp2.Model;
+
+namespace KundeApp2.DAL
+{
+    public interface IObservasjonRepository
+    {
+        Task<bool> Lagre(Observasjon innObservasjon);
+        Task<List<Observasjon>> HentAlle();
+        Task<bool> Slett(int id);
+        Task<Observasjon> HentEn(int id);
+        Task<bool> Endre(Observasjon endreObservasjon);
+        Task<bool> LoggInn(Bruker bruker);
+    }
+}
